5a436cfbaaca3197eaae00c49aff4be12184e864
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2022-12-04 15:00:37 +0530

    feat: updated app.js
    appropriate message will be displayed if bill amount and cash amount is same

--- app.js
+++ app.js
@@ -10,47 +10,42 @@ const returnnotes = document.querySelectorAll(".no-of-notes");
 // cashGiven.style.display ="none";
 
 calculateChange.addEventListener("click", function eventHandler() {
-   hideMessage();
-   
-   var billAmt = Number(billAmount.value);
-   var cashGvn = Number(cashGiven.value);
-   
-    if ((billAmt > 0) && (cashGvn > 0)) {
-        // label.style.display="block";
-        // cashGiven.style.display ="block";
+  hideMessage();
 
-        if (cashGvn >= billAmt){
-            amounttoReturn = cashGvn - billAmt;
-            calculateAmounttobeReturned(amounttoReturn);
+  var billAmt = Number(billAmount.value);
+  var cashGvn = Number(cashGiven.value);
 
-        } 
+  if (billAmt > 0 && cashGvn > 0) {
+    // label.style.display="block";
+    // cashGiven.style.display ="block";
 
-        else{
-
-            showErrorMessage("Do you wanna wash plates? If not, please enter cash amount correctly!");
-            
-        }
-} else {
-        showErrorMessage("Invalid bill amount");
+    if (billAmt === cashGvn) {
+      showErrorMessage("No change to be returned!");
     }
+    if (cashGvn >= billAmt) {
+      amounttoReturn = cashGvn - billAmt;
+      calculateAmounttobeReturned(amounttoReturn);
+    } else {
+      showErrorMessage(
+        "Do you wanna wash plates? If not, please enter cash amount correctly!"
+      );
+    }
+  } else {
+    showErrorMessage("Invalid bill amount");
+  }
 });
 
-function calculateAmounttobeReturned(amount){
-    for ( let i=0; i<noofnotes.length; i++){
-        const note = Math.trunc(amount/noofnotes[i]);
-        amount = amount%noofnotes[i];
-        returnnotes[i].innerText = note;
-    }
-
+function calculateAmounttobeReturned(amount) {
+  for (let i = 0; i < noofnotes.length; i++) {
+    const note = Math.trunc(amount / noofnotes[i]);
+    amount = amount % noofnotes[i];
+    returnnotes[i].innerText = note;
+  }
 }
-function hideMessage(){
-
-    alertMessage.style.display="none";
-
+function hideMessage() {
+  alertMessage.style.display = "none";
 }
-function showErrorMessage(message){
-
-    alertMessage.style.display="block";
-    alertMessage.innerText = message;
-
+function showErrorMessage(message) {
+  alertMessage.style.display = "block";
+  alertMessage.innerText = message;
 }
